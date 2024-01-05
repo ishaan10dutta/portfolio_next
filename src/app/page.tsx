e208a71425426ed972043ce8375731eb02ae780e
Ishaan <ishaan10dutta@gmail.com>
2024-01-05 10:16:51 +0000

    Added project component and projects section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import { Header } from './components/header'
 import { Intro } from './components/intro'
+import SectionDivider from './components/section-divider'
+import About from './components/about'
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center px-4'>
       <Header/>
       <Intro/>
+      <SectionDivider/>
+      <About/>
     </main>
   )
 }
